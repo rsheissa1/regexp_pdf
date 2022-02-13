e015d881d59bc8958f6335d1f38591dea38b522c
rsheissa1 <rocastaneda@uv.mx>
2022-02-13 15:45:02 -0600

    Modificaciones en la estructura de la RegExp. Datos se guardan en un archivo; separados por una coma.

--- test_2.py
+++ test_2.py
@@ -9,60 +9,48 @@ Created on Sat Feb 12 23:12:12 2022
 import re
 
 
-def prueba_linea(linea):
+def lectura_linea(linea):
     
-    print(linea[0:4])
+    #print(linea[0:4])
     
-    if linea[0:4] != "null":
-        linea1 = re.findall(r"([A-Z]*[\w]+[\S]+[A-Z]+[^\:])", linea)
-        linea2 = re.findall(r"([A-Z]+[0-9]+[A-Z]+[0-9]+)", linea)
-        linea3 = re.findall(r"[A-Z]*[a-z]*[0-9_\.-]*@[0-9a-z\.-]+\.[a-z]{2,6}", linea)
-        linea4 = re.findall(r"[a-z]+\://orcid.org/[0-9]+-[0-9]+-[0-9]+-[0-9]+", linea)
-        linea5 = re.findall(r"[A-Z]*[a-z]*[\w]+", linea)
+    #if linea[0:4] != "null":
+    #linea1 = re.findall(r"([A-Z]*[\w]+[\S]+[A-Z]+[^\:])", linea)
+    #linea2 = re.findall(r"([A-Z]+[0-9]+[A-Z]+[0-9]+)", linea)
+    #linea3 = re.findall(r"[A-Z]*[a-z]*[0-9_\.-]*@[0-9a-z\.-]+\.[a-z]{2,6}", linea)
+    #linea4 = re.findall(r"[a-z]+\://orcid.org/[0-9]+-[0-9]+-[0-9]+-[0-9]+", linea)
+    linea5 = re.findall(r"[A-Z]*[a-z]*[\w]+[@a-z\.]*", linea)
     
-        if linea1:
+    #if linea1:
             #print(f"Linea1: {linea1}")
-            pass
+    #    pass
         
-        if linea2:
+    #if linea2:
             #print(f"Linea2: {linea2}")
-            pass
+    #    pass
         
-        if linea3:
+    #if linea3:
             #print(f"Linea3: {linea3}")
-            pass
+    #    pass
         
-        if linea4:
+    #if linea4:
             #print(f"Linea4: {linea4}")
-            pass
-        if linea5:
-            print(f"Linea5: {linea5}")
-    else:
-        #print("Linea con valor null")
-        pass
-
-
-
-
-
-
-#def valida_linea(linea):
-
-#	res1 = re.findall(r"([A-Z]+[a-z]+\s[a-z]+\s[a-z]+\:)",linea)
-#    res2 = re.findall(r"fgjdfk;lgjkld;fjgklsdfgfasdfasfsf", linea)
-
-	#If match is found, the string is valid
-#	if res1:
-		#print(f"Linea {linea} detectada")
-#		print(res1)
+    #    pass
     
- #   if res2:
- #       print(res2)
+    if linea5:
+        #print(f"Linea5: {linea5}")
+        for i in linea5:
+            datos1.write(i + ",")
+    #else:
+        #print("Linea con valor null")
+        #pass
 
 
 if __name__ == "__main__":
+    
+    datos1 = open("generales_filtrados.txt", "a")
     with open("datos_generales.txt", "r", encoding='utf-8') as f:
         for line in f:
-            prueba_linea(line)
-    #print("prueba")       
+            lectura_linea(line)
+    #print("prueba")
+    datos1.close()
     f.close()
